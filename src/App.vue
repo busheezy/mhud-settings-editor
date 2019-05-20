2f1ba76a56a238d316ba9ea414f373040a7fe070
Ryan Bucshon <busheezy@gmail.com>
2019-05-20 13:44:40 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,211 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Disable Alpha:
+    <input type="checkbox" id="checkbox" v-model="disableAlpha" /><br />
+
+    Owner:<br />
+    <input type="text" v-model="settingsInput.owner" /><br />
+
+    Speed Toggle:
+    <input
+      type="checkbox"
+      id="checkbox"
+      v-model="settingsInput.data.speedToggle"
+    /><br />
+
+    Speed Position X:<br />
+    <input
+      type="number"
+      v-model="settingsInput.data.speedPositionX"
+      min="-1"
+      max="1"
+    /><br />
+
+    Speed Position Y:<br />
+    <input
+      type="number"
+      v-model="settingsInput.data.speedPositionY"
+      min="-1"
+      max="1"
+    /><br />
+
+    Speed Normal Color:
+    <Sketch
+      :value="color"
+      :disableAlpha="disableAlpha"
+      disableFields
+      @input="updateValue($event, 'speedNormalColor')"
+    ></Sketch>
+    <br />
+
+    Speed Perf Color:
+    <Sketch
+      :value="color"
+      :disableAlpha="disableAlpha"
+      disableFields
+      @input="updateValue($event, 'speedPerfColor')"
+    ></Sketch>
+    <br />
+
+    Keys Toggle:
+    <input
+      type="checkbox"
+      id="checkbox"
+      v-model="settingsInput.data.keysToggle"
+    />
+    <br />
+
+    Keys Position X:<br />
+    <input
+      type="number"
+      v-model="settingsInput.data.keysPositionX"
+      min="-1"
+      max="1"
+    /><br />
+
+    Keys Position Y:<br />
+    <input
+      type="number"
+      v-model="settingsInput.data.keysPositionY"
+      min="-1"
+      max="1"
+    /><br />
+
+    Keys Normal Color:
+    <Sketch
+      :value="color"
+      :disableAlpha="disableAlpha"
+      disableFields
+      @input="updateValue($event, 'keysNormalColor')"
+    ></Sketch>
+    <br />
+
+    Keys Overlap Color:
+    <Sketch
+      :value="color"
+      :disableAlpha="disableAlpha"
+      disableFields
+      @input="updateValue($event, 'keysOverlapColor')"
+    ></Sketch>
+    <br />
+    <br />
+    <br />
+    <textarea readonly v-model="settingsOutput" class="output"></textarea>
+    <br />
+    <textarea readonly v-model="settingsOutput64" class="output"></textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { Sketch } from 'vue-color';
+import * as simpleVdf from 'simple-vdf2';
+
+const color = { r: 255, g: 0, b: 0, a: 1 };
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld,
+    Sketch,
+  },
+  data() {
+    return {
+      color,
+      settingsInput: {
+        owner: 'bush',
+        data: {
+          speedToggle: false,
+          speedPositionX: 0,
+          speedPositionY: 0,
+          speedNormalColor: color,
+          speedPerfColor: color,
+          speedDisplayFormat: false,
+          keysToggle: false,
+          keysPositionX: 0,
+          keysPositionY: 0,
+          keysNormalColor: color,
+          keysOverlapColor: color,
+        },
+      },
+      disableAlpha: false,
+    };
+  },
+  methods: {
+    updateValue(update, field) {
+      this.settingsInput.data[field] = update.rgba;
+    },
+  },
+  computed: {
+    settingsOutput() {
+      const speedPosition = `${this.settingsInput.data.speedPositionX} ${
+        this.settingsInput.data.speedPositionY
+      }`;
+
+      console.log(JSON.stringify(this.settingsInput, null, 2));
+
+      const speedNormalColor = `${this.settingsInput.data.speedNormalColor.r} ${
+        this.settingsInput.data.speedNormalColor.g
+      } ${this.settingsInput.data.speedNormalColor.b} ${
+        this.settingsInput.data.speedNormalColor.a
+      }`;
+
+      const speedPerfColor = `${this.settingsInput.data.speedPerfColor.r} ${
+        this.settingsInput.data.speedPerfColor.g
+      } ${this.settingsInput.data.speedPerfColor.b} ${
+        this.settingsInput.data.speedPerfColor.a
+      }`;
+
+      const keysPosition = `${this.settingsInput.data.keysPositionX} ${
+        this.settingsInput.data.keysPositionY
+      }`;
+
+      const keysNormalColor = `${this.settingsInput.data.keysNormalColor.r} ${
+        this.settingsInput.data.keysNormalColor.g
+      } ${this.settingsInput.data.keysNormalColor.b} ${
+        this.settingsInput.data.keysNormalColor.a
+      }`;
+
+      const keysOverlapColor = `${this.settingsInput.data.keysOverlapColor.r} ${
+        this.settingsInput.data.keysOverlapColor.g
+      } ${this.settingsInput.data.keysOverlapColor.b} ${
+        this.settingsInput.data.keysOverlapColor.a
+      }`;
+
+      const settingsOutput = {
+        MHud: {
+          owner: this.settingsInput.owner,
+          rev: '1',
+          data: {
+            '0': this.settingsInput.data.speedToggle ? 1 : 0,
+            '1': speedPosition,
+            '2': speedNormalColor,
+            '3': speedPerfColor,
+            '4': this.settingsInput.data.speedDisplayFormat ? 1 : 0,
+            '5': this.settingsInput.data.keysToggle ? 1 : 0,
+            '6': keysPosition,
+            '7': keysNormalColor,
+            '8': keysOverlapColor,
+          },
+        },
+      };
+
+      const outputString = simpleVdf
+        .stringify(settingsOutput, false)
+        .replace(/\r?\n|\r/g, '')
+        .replace(/\t/g, '');
+
+      return outputString;
+    },
+    settingsOutput64() {
+      const base64OutputString = btoa(this.settingsOutput);
+
+      return base64OutputString;
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.output {
+  width: 100%;
+  height: 120px;
 }
 </style>
